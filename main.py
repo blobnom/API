ce3f6d73a4740f518c36da9f5ada10ee719a5ca4
elainaaa <marvintrupp@web.de>
2021-06-19 03:40:42 +0200

    e

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template
 from views.api import api
 
-import os
-
 app = Flask(__name__)
 app.register_blueprint(api, url_prefix='/api')
 
@@ -10,8 +8,7 @@ app.register_blueprint(api, url_prefix='/api')
 def home():
     return render_template('home.html')
 
-#if __name__ == '__main__':
-def app():
-    app.run(debug=True, port=os.getenv('PORT'))
-#else:
-#    print('Please run from main.py')
+if __name__ == '__main__':
+    app.run(debug=True, port=5050)
+else:
+    print('Please run from main.py')
