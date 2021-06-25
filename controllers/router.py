ece0e1a4cfcc6b7382374bf3ba6e2a828236c01c
Elaina <marvintrupp@web.de>
2021-06-25 23:51:38 +0200

    Add endpoints for generation of user top/recent scores

--- controllers/router.py
+++ controllers/router.py
@@ -7,15 +7,21 @@ client = OsuClient(config('OSU_API_KEY'))
 
 class MainHandler(RequestHandler):
 	def get(self):
-		self.write(f"<p>GET /api/user/[username]card<br>" \
-		+ f"GET /api/beatmaps/[map-id]/scores/[username]|~[leaderboard-place]<br>" \
-		+ f"GET /api/beatmaps/[map-id]/card</p>")
+		self.write("<p>" \
+		+ "GET /api/user/[username]card" + "<br>" \
+		+ "GET /api/user/[username]/scores/recent" + "<br>" \
+		+ "GET /api/user/[username]/scores/top" + "<br><br>" \
+		+ "GET /api/beatmaps/[map-id]/scores/[username]|~[leaderboard-place]" + "<br>"\
+		+ "GET /api/beatmaps/[map-id]/card" \
+		+ "</p>")
 		self.set_status(200)
 		self.finish()
 
 router = [
 	(r"/", MainHandler),
 	(r"/api/user/(.*)/card", user.UserCardHandler, dict(client=client)),
+	(r"/api/user/(.*)/scores/top", user.UserTopScoreHandler, dict(client=client)),
+	(r"/api/user/(.*)/scores/recent", user.UserRecentScoreHandler, dict(client=client)),
 	(r"/api/beatmaps/(.*)/card", beatmap.BeatmapCardHandler, dict(client=client)),
 	(r"/api/beatmaps/(.*)/scores/(.*)", beatmap.BeatmapScoreHandler, dict(client=client))
 ]
--- routes/user.py
+++ routes/user.py
@@ -1,8 +1,11 @@
+import os
 import time
 import urllib
 
 from tornado.web import RequestHandler
 from PIL import Image, ImageFont, ImageDraw
+from osutools.utils import Mods
+from osutools.oppai import Oppai
 
 class UserCardHandler(RequestHandler):
 	def initialize(self, client):
@@ -68,4 +71,212 @@ class UserCardHandler(RequestHandler):
 		card.save(f"static/user_cards/{user.id}.png")
 
 		with open(f"static/user_cards/{user.id}.png", "rb") as f:
+			return f.read()
+
+class UserRecentScoreHandler(RequestHandler):
+	def initialize(self, client):
+		self.api = client
+
+	def get(self, username):
+		if username == "":
+			self.set_status(502)
+			self.write({"error": "Please provide a username"})
+
+		u = self.api.fetch_user(username=username)
+		if u is None:
+			self.set_status(404)
+			self.write({"error": "User not found: %s" % username})
+			return
+
+		scores = u.fetch_recent(limit=1)
+		if scores is None:
+			self.set_status(404)
+			self.write({"error": "No scores found"})
+			return
+
+		self.build_image(map, scores[0])
+		image = self.get_image(map, scores[0])
+
+		self.set_header("Content-Type", "image/png")
+		self.set_status(200)
+
+		self.write(image)
+		self.finish()
+
+	def build_image(self, u, score):
+		map = score.fetch_map()
+
+		pp = "{:,.2f}pp".format(score.pp)
+		points = "Score: {:,}".format(score.score)
+		combo = "Combo: {:,}".format(score.max_combo)
+		accuracy = "Accuracy: {:.2f}%".format(score.accuracy_dec * 100)
+		mods = "Mods: {}".format(score.mods)
+		title = "{} [{}]".format(map.song_title, map.difficulty_name)
+		artist = "{}".format(map.artist)
+		player = "{}".format(u.username)
+
+		pp_95 = "95%: {:,.2f}pp".format(
+			Oppai.calculate_pp_from_url(map.download_url, mods=score.mods.value, accuracy=95))
+		pp_97 = "97%: {:,.2f}pp".format(
+			Oppai.calculate_pp_from_url(map.download_url, mods=score.mods.value, accuracy=97))
+		pp_98 = "98%: {:,.2f}pp".format(
+			Oppai.calculate_pp_from_url(map.download_url, mods=score.mods.value, accuracy=98))
+		pp_99 = "99%: {:,.2f}pp".format(
+			Oppai.calculate_pp_from_url(map.download_url, mods=score.mods.value, accuracy=99))
+		pp_100 = "100%: {:,.2f}pp".format(
+			Oppai.calculate_pp_from_url(map.download_url, mods=score.mods.value, accuracy=100))
+
+		# get beatmap background
+		try:
+			urllib.request.urlretrieve(f"https://assets.ppy.sh/beatmaps/{str(map.mapset_id)}/covers/cover.jpg",
+									   f"static/backgrounds/{map.mapset_id}.png")
+		except urllib.error.HTTPError:
+			tmp = Image.new("RGB", (1080, 250), (255, 255, 255))
+			tmp.save(f"static/backgrounds/{map.mapset_id}.png")
+		urllib.request.urlretrieve(f"https://a.ppy.sh/{str(u.id)}?{time.time()}",
+								   f"static/avatars/{u.id}.png")
+
+		bg = Image.open(f"static/backgrounds/{map.mapset_id}.png")
+		bg = bg.resize((1080, 250))
+		avatar = Image.open(f"static/avatars/{u.id}.png")
+		avatar = avatar.resize((128, 128))
+		rank = Image.open("static/ranks/{}.png".format(score.rank))
+		rank = rank.resize((156, 156))
+		card = Image.new('RGBA', (1080, 540), (255, 255, 255, 255))
+		card.paste(bg, (0, 0))
+		card.paste(avatar, (927, 329))
+		card.paste(rank, (75, 350), rank)
+
+		fnt = ImageFont.truetype("static/fonts/arial.ttf", 24)
+		fnt_sml = ImageFont.truetype("static/fonts/arial.ttf", 12)
+		fnt_big = ImageFont.truetype("static/fonts/arial.ttf", 40)
+		d = ImageDraw.Draw(card)
+
+		d.text((5, 255), text=title, font=fnt_big, fill=(0, 0, 0))
+		d.text((5, 300), text=artist, font=fnt, fill=(0, 0, 0))
+		# beatmap creator
+		d.text((931, 486), text=player, font=fnt_sml, fill=(0, 0, 0))
+		d.text((931, 457), text="Played by", font=fnt, fill=(0, 0, 0))
+
+		# score stats
+		d.text((310, 358), text=points, font=fnt, fill=(0, 0, 0))
+		d.text((310, 387), text=combo, font=fnt, fill=(0, 0, 0))
+		d.text((310, 416), text=accuracy, font=fnt, fill=(0, 0, 0))
+		d.text((310, 445), text=mods, font=fnt, fill=(0, 0, 0))
+		d.text((310, 474), text=pp, font=fnt, fill=(0, 0, 0))
+
+		# beatmap pp
+		d.text((605, 358), text=pp_95, font=fnt, fill=(0, 0, 0))
+		d.text((605, 387), text=pp_97, font=fnt, fill=(0, 0, 0))
+		d.text((605, 416), text=pp_98, font=fnt, fill=(0, 0, 0))
+		d.text((605, 445), text=pp_99, font=fnt, fill=(0, 0, 0))
+		d.text((605, 474), text=pp_100, font=fnt, fill=(0, 0, 0))
+		card.save(f"static/scores_recent/{score.user_id}.png")
+
+	def get_image(self, map, score):
+		with open(f"static/scores_recent/{score.user_id}.png", "rb") as f:
+			return f.read()
+
+class UserTopScoreHandler(RequestHandler):
+	def initialize(self, client):
+		self.api = client
+
+	def get(self, username):
+		if username == "":
+			self.set_status(502)
+			self.write({"error": "Please provide a username"})
+
+		u = self.api.fetch_user(username=username)
+		if u is None:
+			self.set_status(404)
+			self.write({"error": "User not found: %s" % username})
+			return
+
+		scores = u.fetch_best(limit=1)
+		if scores is None:
+			self.set_status(404)
+			self.write({"error": "No scores found"})
+			return
+
+		self.build_image(map, scores[0])
+		image = self.get_image(map, scores[0])
+
+		self.set_header("Content-Type", "image/png")
+		self.set_status(200)
+
+		self.write(image)
+		self.finish()
+
+	def build_image(self, u, score):
+		map = score.fetch_map()
+
+		pp = "{:,.2f}pp".format(score.pp)
+		points = "Score: {:,}".format(score.score)
+		combo = "Combo: {:,}".format(score.max_combo)
+		accuracy = "Accuracy: {:.2f}%".format(score.accuracy_dec * 100)
+		mods = "Mods: {}".format(score.mods)
+		title = "{} [{}]".format(map.song_title, map.difficulty_name)
+		artist = "{}".format(map.artist)
+		player = "{}".format(u.username)
+
+		pp_95 = "95%: {:,.2f}pp".format(
+			Oppai.calculate_pp_from_url(map.download_url, mods=score.mods.value, accuracy=95))
+		pp_97 = "97%: {:,.2f}pp".format(
+			Oppai.calculate_pp_from_url(map.download_url, mods=score.mods.value, accuracy=97))
+		pp_98 = "98%: {:,.2f}pp".format(
+			Oppai.calculate_pp_from_url(map.download_url, mods=score.mods.value, accuracy=98))
+		pp_99 = "99%: {:,.2f}pp".format(
+			Oppai.calculate_pp_from_url(map.download_url, mods=score.mods.value, accuracy=99))
+		pp_100 = "100%: {:,.2f}pp".format(
+			Oppai.calculate_pp_from_url(map.download_url, mods=score.mods.value, accuracy=100))
+
+		# get beatmap background
+		try:
+			urllib.request.urlretrieve(f"https://assets.ppy.sh/beatmaps/{str(map.mapset_id)}/covers/cover.jpg",
+									   f"static/backgrounds/{map.mapset_id}.png")
+		except urllib.error.HTTPError:
+			tmp = Image.new("RGB", (1080, 250), (255, 255, 255))
+			tmp.save(f"static/backgrounds/{map.mapset_id}.png")
+		urllib.request.urlretrieve(f"https://a.ppy.sh/{str(u.id)}?{time.time()}",
+								   f"static/avatars/{u.id}.png")
+
+		bg = Image.open(f"static/backgrounds/{map.mapset_id}.png")
+		bg = bg.resize((1080, 250))
+		avatar = Image.open(f"static/avatars/{u.id}.png")
+		avatar = avatar.resize((128, 128))
+		rank = Image.open("static/ranks/{}.png".format(score.rank))
+		rank = rank.resize((156, 156))
+		card = Image.new('RGBA', (1080, 540), (255, 255, 255, 255))
+		card.paste(bg, (0, 0))
+		card.paste(avatar, (927, 329))
+		card.paste(rank, (75, 350), rank)
+
+		fnt = ImageFont.truetype("static/fonts/arial.ttf", 24)
+		fnt_sml = ImageFont.truetype("static/fonts/arial.ttf", 12)
+		fnt_big = ImageFont.truetype("static/fonts/arial.ttf", 40)
+		d = ImageDraw.Draw(card)
+
+		d.text((5, 255), text=title, font=fnt_big, fill=(0, 0, 0))
+		d.text((5, 300), text=artist, font=fnt, fill=(0, 0, 0))
+		# beatmap creator
+		d.text((931, 486), text=player, font=fnt_sml, fill=(0, 0, 0))
+		d.text((931, 457), text="Played by", font=fnt, fill=(0, 0, 0))
+
+		# score stats
+		d.text((310, 358), text=points, font=fnt, fill=(0, 0, 0))
+		d.text((310, 387), text=combo, font=fnt, fill=(0, 0, 0))
+		d.text((310, 416), text=accuracy, font=fnt, fill=(0, 0, 0))
+		d.text((310, 445), text=mods, font=fnt, fill=(0, 0, 0))
+		d.text((310, 474), text=pp, font=fnt, fill=(0, 0, 0))
+
+		# beatmap pp
+		d.text((605, 358), text=pp_95, font=fnt, fill=(0, 0, 0))
+		d.text((605, 387), text=pp_97, font=fnt, fill=(0, 0, 0))
+		d.text((605, 416), text=pp_98, font=fnt, fill=(0, 0, 0))
+		d.text((605, 445), text=pp_99, font=fnt, fill=(0, 0, 0))
+		d.text((605, 474), text=pp_100, font=fnt, fill=(0, 0, 0))
+		card.save(f"static/scores_top/{score.user_id}.png")
+
+	def get_image(self, map, score):
+		with open(f"static/scores_top/{score.user_id}.png", "rb") as f:
 			return f.read()
